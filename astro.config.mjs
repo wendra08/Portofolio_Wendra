151590b14213f6a4962957fd75fb4fb7fb5b4e5f
AhmadLutfiAlfares <ahmadlutfialfares7@gmail.com>
2026-09-10 15:56:15 +0800

    update baru

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   output: 'static',
-  site: 'https://wendra08.github.io',
-  base: '/Portofolio_Wendra',
   devToolbar: { enabled: false },
   vite: { plugins: [tailwindcss()] },
 });
